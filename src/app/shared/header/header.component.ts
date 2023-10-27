5dc219eb70158cfb739963773b6f1dbdc696423c
wilkinjsvsquez <wilkinvsquez@gmail.com>
2023-10-26 20:31:09 -0600

    Login out

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeaderComponent {
+  constructor(private _router: Router, private _userService: UserService) {}
 
+  logout() {
+    this._userService.logout();
+    this._router.navigateByUrl('/login');
+  }
 }
